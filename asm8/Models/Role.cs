53d72cb780bf823f1871374088445cc793d11976
dangkhoa-1999 <khoatdgcd17291@fpt.edu.vn>
2020-10-19 12:08:27 +0700

    update for test

---
+++ asm8/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace asm8.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User{ get; set; }
+    }
+}
---
+++ asm8/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using asm8.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace asm8.Models
+{
+	public class User
+	{
+		public int UserId { get; set; }
+
+		[Required]
+		public int RoleId { get; set; }
+		public Role Role { get; set; }
+	}
+}
